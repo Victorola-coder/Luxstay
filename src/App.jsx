6a889cf9f94c4af70ee9d45aec8b91dfcfd726df
EsinShadrach <esinshadrachrafe@gmail.com>
2023-06-02 16:19:01 +0100

    fixed loading screen bug

--- src/App.jsx
+++ src/App.jsx
@@ -5,47 +5,11 @@ import Navbar from "./components/navbar/navbar";
 function App() {
 	return (
 		<>
-			<Navbar />
+			{/* <Navbar /> */}
 			<Landing />
-			<Footer />
+			{/* <Footer /> */}
 		</>
 	);
 }
 
 export default App;
-
-// const MyComponent = () => {
-// 	const imagePaths = [imageX, imageY, imageZ];
-// 	// const [isLoading, setIsLoading] = useState(true);
-// 	// let { setLoadedFalse } = useContext(ImageContext);
-
-// 	// useEffect(() => {
-// 	// 	const imageLoaders = imagePaths.map((path) => {
-// 	// 		const imageLoader = new Image();
-// 	// 		imageLoader.src = path;
-// 	// 		return new Promise((resolve) => {
-// 	// 			imageLoader.onload = resolve;
-// 	// 		});
-// 	// 	});
-
-// 	// 	Promise.all(imageLoaders).then(() => {
-// 	// 		setLoadedFalse(false)
-// 	// 	});
-// 	// }, []);
-
-// 	// useEffect(()=>{
-// 	// 	setLoadedFalse(false)
-// 	// }, [])
-
-// 	return (
-// 		<div>
-// 			<div>
-// 				<h2>Images Loaded</h2>
-// 				{/* Render your images here */}
-// 				{imagePaths.map((path, index) => (
-// 					<img key={index} src={path} alt={`Image ${index}`} />
-// 				))}
-// 			</div>
-// 		</div>
-// 	);
-// };
--- src/components/main/landing.jsx
+++ src/components/main/landing.jsx
@@ -10,6 +10,8 @@ import deluxe1 from "../../assets/images/deluxeroom1.jpg";
 import executive2 from "../../assets/images/executivelounge2.jpg";
 import standard1 from "../../assets/images/standardroom1.jpg";
 import LoadingScreen from "../../utilities/loadingScreen";
+import Navbar from "../navbar/navbar";
+import Footer from "../footer/footer";
 
 export default function Landing() {
 	const [isLoading, setIsLoading] = useState(true);
@@ -28,18 +30,22 @@ export default function Landing() {
 			setIsLoading(false);
 		});
 	}, []);
-	console.log(isLoading);
+
 	return isLoading ? (
 		<LoadingScreen />
 	) : (
-		<main className="my-6 overflow-x-hidden px-3">
-			<Hero heroImage={heroImage} />
-			<Popular
-				deluxe1={deluxe1}
-				executive2={executive2}
-				standard1={standard1}
-			/>
-			<CallToAction />
-		</main>
+		<>
+			<Navbar />
+			<main className="my-6 overflow-x-hidden px-3">
+				<Hero heroImage={heroImage} />
+				<Popular
+					deluxe1={deluxe1}
+					executive2={executive2}
+					standard1={standard1}
+				/>
+				<CallToAction />
+			</main>
+			<Footer />
+		</>
 	);
 }
