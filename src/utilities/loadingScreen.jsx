6a564e4ff38b2731757ecab1750f99b5f3bfe455
EsinShadrach <esinshadrachrafe@gmail.com>
2023-06-03 09:42:51 +0100

    finally fixed loading screen issues

--- src/utilities/loadingScreen.jsx
+++ src/utilities/loadingScreen.jsx
@@ -26,16 +26,18 @@ export default function LoadingScreen() {
 	}, []);
 
 	return (
-		<section className="flex flex-col items-center justify-center min-h-screen gap-5 px-3 bg-teal-100/20 fixed inset-0">
-			<div>
-				<img src={logo} alt="" className="w-28 h-28 animate-pulse" />
-			</div>
-			<div className="bg-teal-500 w-full max-w-md mx-auto rounded-md p-1 px-1 relative border-2 border-teal-200">
-				<div className="absolute font-bold text-teal-200 left-0 right-0 flex items-center justify-center">
-					{Math.round(progress)}%
+		<section className='bg-white fixed w-full inset-0 z-50'>
+			<section className="flex flex-col items-center justify-center min-h-screen gap-5 px-3 bg-teal-100/20">
+				<div>
+					<img src={logo} alt="" className="w-28 h-28 animate-pulse" />
 				</div>
-				<div className="h-6 font-bold text-teal-200 bg-teal-900 loadingAnimation flex items-center justify-center p-1" />
-			</div>
+				<div className="bg-teal-500 w-full max-w-md mx-auto rounded-md p-1 px-1 relative border-2 border-teal-200">
+					<div className="absolute font-bold text-teal-200 left-0 right-0 flex items-center justify-center">
+						{Math.round(progress)}%
+					</div>
+					<div className="h-6 font-bold text-teal-200 bg-teal-900 loadingAnimation flex items-center justify-center p-1" />
+				</div>
+			</section>
 		</section>
 	);
 }
