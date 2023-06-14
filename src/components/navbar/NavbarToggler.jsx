d52f0b4d9c1ddfc61e593d4abbaf172e17bebeb0
Esin Shadrach Asukwo <esinshadrachrafe@gmail.com>
2023-06-14 14:51:19 +0100

    fixed navbar soze ossues

--- src/components/navbar/NavbarToggler.jsx
+++ src/components/navbar/NavbarToggler.jsx
@@ -8,9 +8,9 @@ export function NavbarToggler({ setIsOpened, isOpened }) {
 				onClick={() => setIsOpened(!isOpened)}
 			>
 				<div
-					className={`transition-all duration-300 bg-black h-0.5 w-7 ${isOpened
+					className={`transition-all duration-300 bg-black h-0.5 ${isOpened
 							? "rotate-45 translate-y-[0.26rem] w-6"
-							: "translate-y-0 rotate-0"}`} />
+							: "translate-y-0 rotate-0 w-7"}`} />
 				<div
 					className={`transition-all duration-300 bg-black h-0.5 w-4 ${isOpened ? "hidden" : "block"}`} />
 				<div
