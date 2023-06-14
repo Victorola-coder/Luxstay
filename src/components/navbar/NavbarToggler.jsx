ef7ff3fabd07a1cc446369e41960b3a142f16a17
Esin Shadrach Asukwo <esinshadrachrafe@gmail.com>
2023-06-14 14:43:35 +0100

    edited nav toggler

--- src/components/navbar/NavbarToggler.jsx
+++ src/components/navbar/NavbarToggler.jsx
@@ -2,22 +2,35 @@ import React from "react";
 
 export function NavbarToggler({ setIsOpened, isOpened }) {
 	return (
-		<div className="md:hidden max-md:block relative z-10">
-			<button
-				className="space-y-1.5"
-				onClick={() => setIsOpened(!isOpened)}
-			>
-				<div
-					className={`transition-all duration-300 bg-black h-0.5 w-5 ${isOpened
-							? "rotate-45 translate-y-[0.26rem] w-6"
-							: "translate-y-0 rotate-0"}`} />
-				<div
-					className={`transition-all duration-300 bg-black h-0.5 w-6 ${isOpened ? "hidden" : "block"}`} />
-				<div
-					className={`transition-all duration-300 bg-black h-0.5 w-7 ${isOpened
-							? "-rotate-45 -translate-y-[0.24rem] w-6"
-							: "translate-y-0 rotate-0"}`} />
-			</button>
-		</div>
+		<div className="w-10 flex justify-center items-center md:hidden">
+					<button
+						className="space-y-1.5 flex flex-col items-end self-end"
+						onClick={()=>setIsOpened(!isOpened)}
+					>
+						<div
+							className={`transition-all duration-300 bg-white/75 h-0.5 w-6 ${
+								isOpened
+									? "rotate-45 translate-y-[0.26rem]"
+									: "translate-y-0 rotate-0"
+							}`}
+						/>
+						<div
+							className={`transition-all duration-300 bg-white/75 h-0.5 w-8 ${
+								isOpened ? "hidden" : "block"
+							}`}
+						/>
+						<div
+							className={`transition-all duration-300 bg-white/75 h-0.5 ${
+								isOpened
+									? "-rotate-45 -translate-y-[0.24rem] w-6"
+									: "translate-y-0 rotate-0 w-4"
+							}`}
+						/>
+					</button>
+				</div>
 	);
 }
+
+
+// isOpened
+isOpened
