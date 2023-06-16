be3ea3e281d704ed8b78979dc5e76ec42261ffea
EsinShadrach <esinshadrachrafe@gmail.com>
2023-06-16 20:32:40 +0100

    completed cursor follow

--- src/components/utilities/CursorCircle.jsx
+++ src/components/utilities/CursorCircle.jsx
@@ -1,8 +1,12 @@
 import React, { useState, useEffect } from "react";
 
 const CursorCircle = () => {
-	const [position, setPosition] = useState({ x: 0, y: 0 });
+	const [position, setPosition] = useState({
+		x: window.innerWidth / 2,
+		y: window.innerHeight / 2,
+	});
 	const [isIdle, setIsIdle] = useState(false);
+	const [isInitialLoad, setIsInitialLoad] = useState(true);
 
 	let timeoutId;
 
@@ -10,9 +14,10 @@ const CursorCircle = () => {
 		clearTimeout(timeoutId);
 		setPosition({ x: event.clientX, y: event.clientY });
 		setIsIdle(false);
+		setIsInitialLoad(false);
 		timeoutId = setTimeout(() => {
 			setIsIdle(true);
-		}, 1000);
+		}, 1000); // Adjust the delay duration as needed
 	};
 
 	useEffect(() => {
@@ -25,12 +30,18 @@ const CursorCircle = () => {
 	}, []);
 
 	return (
-		<div
-			className={`fixed z-50 top-0 left-0 w-${isIdle ? "6" : "3"} h-${
-				isIdle ? "6" : "3"
-			} bg-verdigris/50 rounded-full pointer-events-none transition-all duration-300`}
-			style={{ transform: `translate(${position.x}px, ${position.y}px)` }}
-		></div>
+		<>
+			<div
+				className={`fixed z-50 transform -translate-x-1/2 -translate-y-1/2 ${
+					isIdle
+						? "h-6 w-6"
+						: isInitialLoad
+						? "h-10 w-10 animate-pulse"
+						: "h-4 w-4"
+				} bg-verdigris/50 rounded-full pointer-events-none transition-all duration-300`}
+				style={{ left: `${position.x}px`, top: `${position.y}px` }}
+			></div>
+		</>
 	);
 };
 
