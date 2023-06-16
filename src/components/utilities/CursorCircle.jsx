556d5673a9ae0b223790baa722296afec20c28e2
EsinShadrach <esinshadrachrafe@gmail.com>
2023-06-16 20:05:10 +0100

    added stop detector

--- src/components/utilities/CursorCircle.jsx
+++ src/components/utilities/CursorCircle.jsx
@@ -2,24 +2,35 @@ import React, { useState, useEffect } from "react";
 
 const CursorCircle = () => {
 	const [position, setPosition] = useState({ x: 0, y: 0 });
+	const [isIdle, setIsIdle] = useState(false);
+
+	let timeoutId;
+
+	const handleMouseMove = (event) => {
+		clearTimeout(timeoutId);
+		setPosition({ x: event.clientX, y: event.clientY });
+		setIsIdle(false);
+		timeoutId = setTimeout(() => {
+			setIsIdle(true);
+		}, 1000);
+	};
 
 	useEffect(() => {
-		const handleMouseMove = (event) => {
-			setPosition({ x: event.clientX, y: event.clientY });
-		};
-
 		window.addEventListener("mousemove", handleMouseMove);
 
 		return () => {
 			window.removeEventListener("mousemove", handleMouseMove);
+			clearTimeout(timeoutId);
 		};
 	}, []);
 
 	return (
 		<div
-			className="fixed top-0 left-0 w-6 h-6 bg-verdigris/50 rounded-full pointer-events-none z-50"
+			className={`fixed z-50 top-0 left-0 w-${isIdle ? "6" : "3"} h-${
+				isIdle ? "6" : "3"
+			} bg-verdigris/50 rounded-full pointer-events-none transition-all duration-300`}
 			style={{ transform: `translate(${position.x}px, ${position.y}px)` }}
-		/>
+		></div>
 	);
 };
 
