d867101abf139bf91f4888ecb944c75f07b479f7
EsinShadrach <esinshadrachrafe@gmail.com>
2023-06-16 23:12:22 +0100

    fixed routing issues in Link

--- src/components/pages/booking/bookingPage/Bookings.jsx
+++ src/components/pages/booking/bookingPage/Bookings.jsx
@@ -12,7 +12,7 @@ export default function Bookings() {
 				</h1>
 				<div className="grid md:grid-cols-2 lg:grid-cols-3 gap-5 my-5 place-content-center">
 					{cards.map((card, index) => (
-						<Link key={index} to={`booking/${card.id}`}>
+						<Link key={index} to={`${index + 1}/`}>
 							<Card {...card} />
 						</Link>
 					))}
