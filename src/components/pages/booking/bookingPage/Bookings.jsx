85f583d5ac454088e1fde8e0ef113578c8499236
EsinShadrach <esinshadrachrafe@gmail.com>
2023-06-17 00:56:18 +0100

    fixed sizing issues for mobile

--- src/components/pages/booking/bookingPage/Bookings.jsx
+++ src/components/pages/booking/bookingPage/Bookings.jsx
@@ -10,7 +10,7 @@ export default function Bookings() {
 				<h1 className="text-center font-bold text-4xl text-amber-900">
 					Booking
 				</h1>
-				<div className="grid md:grid-cols-2 lg:grid-cols-3 gap-5 my-5 place-content-center">
+				<div className="grid md:grid-cols-2 lg:grid-cols-3 gap-5 my-5 md:place-content-center">
 					{cards.map((card, index) => (
 						<Link key={index} to={`${index + 1}/`}>
 							<Card {...card} />
