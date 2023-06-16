4de9b2936301501c87209b15ff5dc6df7e914ca0
EsinShadrach <esinshadrachrafe@gmail.com>
2023-06-16 21:05:27 +0100

    completed ripple effect onMouseDown

--- src/components/utilities/CursorCircle.jsx
+++ src/components/utilities/CursorCircle.jsx
@@ -7,8 +7,10 @@ const CursorCircle = () => {
 	});
 	const [isIdle, setIsIdle] = useState(false);
 	const [isInitialLoad, setIsInitialLoad] = useState(true);
+	const [mousePressed, setMousePressed] = useState(false);
 
 	let timeoutId;
+	let rippleTimeOut;
 
 	const handleMouseMove = (event) => {
 		clearTimeout(timeoutId);
@@ -17,22 +19,32 @@ const CursorCircle = () => {
 		setIsInitialLoad(false);
 		timeoutId = setTimeout(() => {
 			setIsIdle(true);
-		}, 1000); // Adjust the delay duration as needed
+		}, 1000);
 	};
 
 	useEffect(() => {
 		window.addEventListener("mousemove", handleMouseMove);
 
+		window.addEventListener("mousedown", mouseAction);
+
 		return () => {
 			window.removeEventListener("mousemove", handleMouseMove);
+			window.removeEventListener("mousedown", mouseAction);
 			clearTimeout(timeoutId);
 		};
-	}, []);
+	}, [mousePressed]);
+
+	function mouseAction() {
+		setMousePressed(true);
+		rippleTimeOut = setTimeout(() => {
+			setMousePressed(false);
+		}, 1000);
+	}
 
 	return (
 		<>
 			<div
-				className={`fixed z-50 transform -translate-x-1/2 -translate-y-1/2 ${
+				className={` fixed z-50 transform -translate-x-1/2 -translate-y-1/2 ${
 					isIdle
 						? "h-6 w-6"
 						: isInitialLoad
@@ -40,7 +52,13 @@ const CursorCircle = () => {
 						: "h-4 w-4"
 				} bg-verdigris/50 rounded-full pointer-events-none transition-all duration-300`}
 				style={{ left: `${position.x}px`, top: `${position.y}px` }}
-			></div>
+			/>
+			{mousePressed && (
+				<div
+					className="ripple fixed h-[100px] w-[100px] rounded-full bg-verdigris/30 z-50 pointer-events-none transition-all duration-300"
+					style={{ left: `${position.x}px`, top: `${position.y}px` }}
+				/>
+			)}
 		</>
 	);
 };
